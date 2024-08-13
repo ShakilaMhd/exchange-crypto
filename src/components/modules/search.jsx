c9c5985f9c785f2e8a67350fa9c63c7679b39ab8
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-13 16:59:47 +0330

    add search bar

--- src/components/modules/search.jsx
+++ src/components/modules/search.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { searchCoin } from "../../services/CryptoApi";
 
-function search({currency, setCurrency}) {
+function search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coin, setCoin] = useState([]);
+
+  useEffect(() => {
+    //   console.log(text);
+    // console.log("object");
+    if (!text) return;
+    const search = async () => {
+      const res = await fetch(searchCoin(text));
+      const json = await res.json();
+      console.log(json);
+      if (json.coins) setCoin(json.coins);
+    };
+    search();
+  }, [text]);
+
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -1,8 +1,12 @@
-const BASE_URL = "https://api.coingecko.com/api/v3/coins";
+const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-ANWjXMCWFzwHACqqhkpaSuX5"
 
 const getCoinList = (page, currency) => {
-    return `${BASE_URL}/markets?vs_currency=${currency}&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${API_KEY}`;
+    return `${BASE_URL}/coins/markets?vs_currency=${currency}&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 }
 
-export { getCoinList }
+const searchCoin = (query) => {
+    return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`
+}
+
+export { getCoinList, searchCoin }
