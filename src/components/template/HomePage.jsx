8604acd54ad7f3e1b77c7bb326fc6154fbbf2d5f
shakila mohammadi <57131482+ShakilaMhd@users.noreply.github.com>
2024-08-13 15:58:32 +0330

    add the currency for market

--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import TableCoins from "../modules/TableCoins";
 import { getCoinList } from "../../services/CryptoApi";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/search"
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd")
 
   useEffect(() => {
     // const options = { method: "GET", headers: { accept: "application/json" } };
@@ -17,15 +19,16 @@ function HomePage() {
 
     const getData = async () => {
       setIsLoading(true);
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, [page]);
+  }, [page, currency]);
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency} />
       <TableCoins coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>
--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://api.coingecko.com/api/v3/coins";
 const API_KEY = "CG-ANWjXMCWFzwHACqqhkpaSuX5"
 
-const getCoinList = (page) => {
-    return `${BASE_URL}/markets?vs_currency=usd&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${API_KEY}`;
+const getCoinList = (page, currency) => {
+    return `${BASE_URL}/markets?vs_currency=${currency}&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 }
 
 export { getCoinList }
